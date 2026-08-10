f8b34d3cd2d412b07034bdab8cee2672d879d670
karimob67 <75337335+karimob67@users.noreply.github.com>
2026-08-10 15:40:33 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'simu';
+$username = 'root';   // نام کاربری دیتابیس خود را وارد کنید
+$password = '';       // رمز عبور دیتابیس خود را وارد کنید
 
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("خطا در اتصال به دیتابیس: " . $e->getMessage());
+}
+?>
